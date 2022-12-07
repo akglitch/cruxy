ca6023a90ff3a0582417a989797110ded78cb72d
kaytee <mcaninakwa@gmail.com>
2022-12-06 20:59:06 -0500

    navbar update

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,91 +1,123 @@
-import { Input, Grid, Image, Card, Spacer, Text, Row, Textarea, Container } from "@nextui-org/react"
+import { Input, Grid, Image, Button, Card, Spacer, Text, Row, Textarea, Container } from "@nextui-org/react"
+import { useState } from "react"
+
+import axios from "axios";
+
+
 
 export const Hero = () => {
+
+
+
+
+    const [productName, setProductName] = useState('')
+    const [productCategory, setProductCategory] = useState('')
+    const [productPrice, setProductPrice] = useState(0)
+    const [productDescription, setProductDescription] = useState('')
+
+    const addToList = () => {
+        axios.post("http://localhost:3001/post", {
+            name: productName,
+            category: productCategory,
+            price: productPrice,
+            description: productDescription,
+        })
+    }
+
+
     return (
         <Container>
-        <Grid.Container>
+            <Grid.Container>
+
+                <Grid xs={12} sm={6} md={6} lg={6}>
+                    <Image
+                        src="form.svg"
+                        css={{ borderRadius: "$2xl" }}
+                        alt={"con"}
+                    />
+                </Grid>
+
+
+                <Grid xs={12} sm={6} md={6} lg={6} justify={"center"} >
+
+                    <Card css={{ p: "$6", padding: "$10", marginTop: "$28", mw: "650px" }}
+                        variant={"bordered"}>
+
+                        <Card.Header>
+                            <Text b color="primary" id="Card-title" size={18} justify={"center"}>
+                                Enter product details
+                            </Text>
+                        </Card.Header>
+                        <Card.Body>
+                            <Input
+                                aria-label="name"
+                                clearable
+                                underlined
+                                required
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                placeholder="Name"
+                                name="product_name"
+                                onChange={(e) => {
+                                    setProductName(e.target.value);
+                                }}
+                            />
+                            <Spacer y={3} />
+                            <Input
+                                aria-label="cagegory"
+                                clearable
+                                required
+                                underlined
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                placeholder="Category"
+                                name="product_category"
+                                onChange={(e) => {
+                                    setProductCategory(e.target.value);
+                                }}
+                            />
+                            <Spacer y={3} />
+                            <Input
+                                aria-label="price"
+                                clearable
+                                required
+                                underlined
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                placeholder="Price"
+                                name="product_price"
+                                onChange={(e) => {
+                                    setProductPrice(e.target.value);
+                                }}
+                            />
+                            <Spacer y={3} />
+                            <Textarea
+                                underlined
+                                aria-label="description"
+                                color="primary"
+                                placeholder="productDescription"
+                                name="description"
+                                onChange={(e) => {
+                                    setProductDescription(e.target.value);
+                                }}
+                            />
+                            <Row></Row>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button auto color="gradient" rounded bordered onClick={addToList}>Add to List</Button>
+                        </Card.Footer>
+
+                    </Card>
+
+
+                </Grid>
 
 
 
-
-
-        <Grid xs={12} sm={6} md={6} lg={6}>
-                <Image
-                    src="form.svg"
-                    css={{ borderRadius: "$2xl" }}
-                    alt={"con"}
-                />
-            </Grid>
-
-            
-            <Grid xs={12} sm={6} md={6} lg={6} justify={"center"} >
-
-                <Card css={{ p: "$6", padding: "$10", marginTop: "$28", mw: "650px" }}
-                    variant={"bordered"}>
-
-                    <Card.Header>
-                        <Text b color="primary" id="Card-title" size={18} justify={"center"}>
-                            Enter product details
-                        </Text>
-                    </Card.Header>
-                    <Card.Body>
-                        <Input
-                            aria-label="name"
-                            clearable
-                            underlined
-                            required
-                            fullWidth
-                            color="primary"
-                            size="lg"
-                            placeholder="Name"
-                            name="product_name"
-                        />
-                        <Spacer y={3} />
-                        <Input
-                            aria-label="cagegory"
-                            clearable
-                            required
-                          underlined
-                            fullWidth
-                            color="primary"
-                            size="lg"
-                            placeholder="Category"
-                            name="product_category"
-                        />
-                        <Spacer y={3} />
-                        <Input
-                            aria-label="price"
-                            clearable
-                            required
-                          underlined
-                            fullWidth
-                            color="primary"
-                            size="lg"
-                            placeholder="Price"
-                            name="product_price"
-                        />
-                        <Spacer y={3} />
-                        <Textarea
-                          underlined
-                            aria-label="description"
-                            color="primary"
-                            placeholder="productDescription"
-                            name="description"
-                        />
-                        <Row></Row>
-                    </Card.Body>
-                    <Card.Footer>
-
-                    </Card.Footer>
-
-                </Card>
-
-
-            </Grid>
-
-
-
-        </Grid.Container>
+            </Grid.Container>
         </Container>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,117 +1,117 @@
 import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
 import { AcmeLogo } from "./AcmeLogo.js";
 
-export const Nav = () =>{
-    const collapseItems = [
-        "Profile",
-        "Dashboard",
-       
-        "Log Out",
-      ];
-    
-    return(
-      
-        <Navbar isBordered variant="sticky" >
-          <Navbar.Toggle showIn="xs" />
-          <Navbar.Brand
-            css={{
-              "@xs": {
-                w: "12%",
-              },
-            }}
-          >
-            <AcmeLogo />
-            <Text b color="inherit" hideIn="xs">
-              ACME
-            </Text>
-          </Navbar.Brand>
-          <Navbar.Content
-            enableCursorHighlight
-            activeColor="warning"
-            hideIn="xs"
-            variant="highlight"
-          >
-            <Navbar.Link href="#">Home</Navbar.Link>
-            <Navbar.Link isActive href="#">
-              Products
-            </Navbar.Link>
-            <Navbar.Link href="#">Projects</Navbar.Link>
-            <Navbar.Link href="#">Portfolio</Navbar.Link>
-          </Navbar.Content>
-          <Navbar.Content
-            css={{
-              "@xs": {
-                w: "12%",
-                jc: "flex-end",
-              },
-            }}
-          >
-            <Dropdown placement="bottom-right">
-              <Navbar.Item>
-                <Dropdown.Trigger>
-                  <Avatar
-                    bordered
-                    as="button"
-                    color="warning"
-                    size="md"
-                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                  />
-                </Dropdown.Trigger>
-              </Navbar.Item>
-              <Dropdown.Menu
-                aria-label="User menu actions"
+export const Nav = () => {
+  const collapseItems = [
+    "Profile",
+    "Dashboard",
+
+    "Log Out",
+  ];
+
+  return (
+
+    <Navbar isBordered variant="sticky" >
+      <Navbar.Toggle showIn="xs" />
+      <Navbar.Brand
+        css={{
+          "@xs": {
+            w: "12%",
+          },
+        }}
+      >
+        <AcmeLogo />
+        <Text b color="inherit" hideIn="xs">
+          ACME
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content
+        enableCursorHighlight
+        activeColor="warning"
+        hideIn="xs"
+        variant="highlight"
+      >
+        <Navbar.Link href="#">Home</Navbar.Link>
+        <Navbar.Link isActive href="#">
+          Products
+        </Navbar.Link>
+        <Navbar.Link href="#">Projects</Navbar.Link>
+        <Navbar.Link href="#">Portfolio</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content
+        css={{
+          "@xs": {
+            w: "12%",
+            jc: "flex-end",
+          },
+        }}
+      >
+        <Dropdown placement="bottom-right">
+          <Navbar.Item>
+            <Dropdown.Trigger>
+              <Avatar
+                bordered
+                as="button"
                 color="warning"
-                onAction={(actionKey) => console.log({ actionKey })}
-              >
-                <Dropdown.Item key="profile" css={{ height: "$18" }}>
-                  <Text b color="inherit" css={{ d: "flex" }}>
-                    Signed in as
-                  </Text>
-                  <Text b color="inherit" css={{ d: "flex" }}>
-                    zoey@example.com
-                  </Text>
-                </Dropdown.Item>
-                <Dropdown.Item key="settings" withDivider>
-                  My Settings
-                </Dropdown.Item>
-                <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
-                <Dropdown.Item key="analytics" withDivider>
-                  Analytics
-                </Dropdown.Item>
-                <Dropdown.Item key="system">System</Dropdown.Item>
-                <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
-                <Dropdown.Item key="help_and_feedback" withDivider>
-                  Help & Feedback
-                </Dropdown.Item>
-                <Dropdown.Item key="logout" withDivider color="error">
-                  Log Out
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Navbar.Content>
-          <Navbar.Collapse disableAnimation>
-            {collapseItems.map((item, index) => (
-              <Navbar.CollapseItem
-                key={item}
-                activeColor="warning"
-                css={{
-                  color: index === collapseItems.length - 1 ? "$error" : "",
-                }}
-                isActive={index === 2}
-              >
-                <Link
-                  color="inherit"
-                  css={{
-                    minWidth: "100%",
-                  }}
-                  href="#"
-                >
-                  {item}
-                </Link>
-              </Navbar.CollapseItem>
-            ))}
-          </Navbar.Collapse>
-        </Navbar>
-    
-    )
+                size="md"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </Dropdown.Trigger>
+          </Navbar.Item>
+          <Dropdown.Menu
+            aria-label="User menu actions"
+            color="warning"
+            onAction={(actionKey) => console.log({ actionKey })}
+          >
+            <Dropdown.Item key="profile" css={{ height: "$18" }}>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                Signed in as
+              </Text>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                zoey@example.com
+              </Text>
+            </Dropdown.Item>
+            <Dropdown.Item key="settings" withDivider>
+              My Settings
+            </Dropdown.Item>
+            <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+            <Dropdown.Item key="analytics" withDivider>
+              Analytics
+            </Dropdown.Item>
+            <Dropdown.Item key="system">System</Dropdown.Item>
+            <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+            <Dropdown.Item key="help_and_feedback" withDivider>
+              Help & Feedback
+            </Dropdown.Item>
+            <Dropdown.Item key="logout" withDivider color="error">
+              Log Out
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar.Content>
+      <Navbar.Collapse disableAnimation>
+        {collapseItems.map((item, index) => (
+          <Navbar.CollapseItem
+            key={item}
+            activeColor="warning"
+            css={{
+              color: index === collapseItems.length - 1 ? "$error" : "",
+            }}
+            isActive={index === 2}
+          >
+            <Link
+              color="inherit"
+              css={{
+                minWidth: "100%",
+              }}
+              href="#"
+            >
+              {item}
+            </Link>
+          </Navbar.CollapseItem>
+        ))}
+      </Navbar.Collapse>
+    </Navbar>
+
+  )
 }
