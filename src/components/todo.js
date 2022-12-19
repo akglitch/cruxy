60dc15a34e821fb564fa96c4086808d20c050396
kaytee <mcaninakwa@gmail.com>
2022-12-18 21:02:19 -0500

    code under update button

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
-import { Grid ,Card,Button,Text,Row, Container} from "@nextui-org/react";
+import { Grid ,Card,Button,Text,Row,Input, Container} from "@nextui-org/react";
 
  export const Todo = () => {
 
     const [todoList, setTodoList] = useState([])
+    const [newTitle, setNewTitle] = useState('')
+    const [newDescription, setNewDescription] = useState('')
 
     const getTodos = () => {
       axios.get("http://localhost:3001/todos").then((response) =>{
@@ -24,6 +26,13 @@ import { Grid ,Card,Button,Text,Row, Container} from "@nextui-org/react";
     }
 
 
+    const updateTodo =(id)=>{
+      axios.put(`http://localhost:3001/update/${id}`,{
+      title : newTitle,
+      description :newDescription,
+    })
+    }
+
     return(
       <Container lg>
         <div justify="center" >
@@ -42,13 +51,39 @@ import { Grid ,Card,Button,Text,Row, Container} from "@nextui-org/react";
             <Text>
              Description: {val.description}
              </Text>
+             <Input
+                        clearable
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="lg"
+                        placeholder="NewTitle"
+                        onChange={(e) =>  {
+                            setNewTitle(e.target.value)
+                        }}
+                    />
+                     <Input
+                        clearable
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="lg"
+                        placeholder="newDescription"
+                        onChange={(e) =>  {
+                            setNewDescription(e.target.value)
+                        }}
+
+                    />
+                    
+                    
           </Card.Body>
          
           <Card.Footer>
             <Row justify="flex-end">
-              <Button color="primary"flat size="xs">
+              <Button color="primary"flat size="xs" onPress={() => updateTodo(val._id)}>
             update
               </Button>
+            
               <Button color="error"flat size="xs" onPress={() => deleteTodo(val._id)}>delete</Button>
             </Row>
           </Card.Footer>
